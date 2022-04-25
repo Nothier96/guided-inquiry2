42e5a56183766c5801a44a91e0d6a6457517d3c9
Thierno Sissoko <thiernosissoko@AdministorsMBP2.attlocal.net>
2022-04-25 15:07:23 -0400

    pushing easy

---
+++ easy/script.js
@@ -0,0 +1,7 @@
+const student1 = ["Thierno", "Sissoko", 25];
+const student2 = ["John", "Doe", 19];
+const student3 = ["Jodi", "Pierre", 27];
+const students = [student1, student2, student3];
+console.log(
+  `Hello , my name is ${students[1][0]} ${students[1][1]} and I am ${students[1][2]} years old`
+);
